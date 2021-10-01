bb7ee79c6e76ddd3f266f7d2558c8b45301c6adb
Ketil Malde <ketil@malde.org>
2021-10-01 14:53:43 +0200

    Add code to work with data and model repos

--- Project.py
+++ Project.py
@@ -2,38 +2,54 @@
 
 import os
 import os.path
+import sys
 
-model='git:...'
-dataset='git:...'
+model='git://no.such.server/Pytorch-UNet'
+dataset='git://no.such.server/blue-mussel-drone-testdata'
 
 class Project:
 
-    def setup():
+    def __init__(self):
+        self.m = None
+        self.d = None
+        self.config = {}
+
+    def setup(self):
         '''Get data and model repositories'''
         os.system(f'git clone {model}')
-        sys.path.insert(1, os.path.basename(dataset))
+        self.config['modelpath'] = os.path.basename(model)
+        sys.path.insert(1, self.config['modelpath'])
         import Model
         self.m = Model.Model()
-        os.path.remove(os.path.basename(dataset))
+        sys.path.remove(self.config['modelpath'])
 
         os.system(f'git clone {dataset}')
-        sys.path.insert(1, os.path.basename(dataset))
+        self.config['datapath'] = os.path.basename(dataset)
+        sys.path.insert(1, self.config['datapath'])
         import Data
         self.d = Data.Data()
-        os.path.remove(os.path.basename(dataset))
+        sys.path.remove(self.config['datapath'])
 
-    def get_data():
+    def get_data(self):
         '''Download and verify data'''
-        d.get()
-        d.validate()
+        self.d.get(self.config)
+        self.d.validate(self.config)
 
-    def build_model():
+    def build_model(self):
         '''Build the model and check it'''
-        sys.path.insert(os.path.basename(model))
-        m.build()
-        m.check()
+        self.m.build(self.config)
+        self.m.check(self.config)
 
-    def train_model():
+    def train_model(self):
         '''Train the model'''
-        m.train()
+        self.m.train(self.config)
 
+
+if __name__ == '__main__':
+    p = Project()
+    p.setup()
+    p.build_model()
+    p.get_data()
+    p.train_model()
+
+    print(dir(p))
