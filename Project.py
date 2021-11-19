58cd03153c9029d50adbe16ae4d4ebd401f3420d
Ketil Malde <ketil@malde.org>
2021-11-19 15:23:32 +0100

    Update with local config storage in submodules

--- Project.py
+++ Project.py
@@ -7,49 +7,55 @@ import sys
 model='--branch standardize https://github.com/ketil-malde/Pytorch-UNet'
 dataset='git://github.com/ketil-malde/blue-mussel-drone-testdata'
 
+# default configuration
+config = {
+    'runtime': '--gpus device=0'  # or just '' for CPU
+}
+
 class Project:
 
-    def __init__(self):
+    def __init__(self, conf):
         self.m = None
         self.d = None
-        self.config = {}
+        self.config = conf
 
     def setup(self):
         '''Get data and model repositories'''
         os.system(f'git clone {model}')
         self.config['modelpath'] = os.path.basename(model)
-        sys.path.insert(1, self.config['modelpath'])
-        import Model
-        self.m = Model.Model()
-        sys.path.remove(self.config['modelpath'])
-
         os.system(f'git clone {dataset}')
         self.config['datapath'] = os.path.basename(dataset)
+
+        sys.path.insert(1, self.config['modelpath'])
+        import Model
+        self.m = Model.Model(self.config)
+        sys.path.remove(self.config['modelpath'])
+
         sys.path.insert(1, self.config['datapath'])
         import Data
-        self.d = Data.Data()
+        self.d = Data.Data(self.config)
         sys.path.remove(self.config['datapath'])
 
     def get_data(self):
         '''Download and verify data'''
-        self.d.get(self.config)
-        self.d.validate(self.config)
+        self.d.get()
+        self.d.validate()
 
     def build_model(self):
         '''Build the model and check it'''
-        self.m.build(self.config)
-        self.m.check(self.config)
+        self.m.build()
+        self.m.check()
 
     def train_model(self):
         '''Train the model'''
-        self.m.train(self.config)
+        self.m.train()
 
 
 if __name__ == '__main__':
-    p = Project()
+    p = Project(config)
     p.setup()
     p.build_model()
-    p.get_data()
+    # p.get_data()
     p.train_model()
 
     print(dir(p))
